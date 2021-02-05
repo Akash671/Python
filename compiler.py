fec57e99c3c33c691209fade02edf20c62ea9c76
Akash671 <akashsaini454545@gmail.com>
2021-02-05 22:12:59 +0530

    compiler

---
+++ compiler.py
@@ -0,0 +1,8 @@
+import webbrowser as we 
+
+url1="https://www.programiz.com/python-programming/online-compiler/"
+
+we.open(url1)
+url2="https://www.programiz.com/cpp-programming/online-compiler/"
+
+we.open(url2)
